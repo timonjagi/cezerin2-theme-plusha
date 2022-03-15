23a9234777c7fb09a6438a0581b7611151af8277
Bingwa <timothy.njagi@gmail.com>
2022-03-15 12:44:54 +0300

    first commit

---
+++ assets/precache-manifest.94d15d3c6ffe78f7ed6019745a34128f.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/js/app-bb0ecba92ff26365f5b8.js"
+  },
+  {
+    "revision": "64546219f427965a1e57",
+    "url": "/assets/css/bundle-1b91c4f5b62de16c1de3.css"
+  },
+  {
+    "url": "/assets/js/theme-64546219f427965a1e57.js"
+  }
+];
